493fbbaa6badfba71ce8f353e44db619ac655632
Meesam <meesam.engineer@gmail.com>
2015-09-13 15:06:34 +0530

    Add public folder for view.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser=require("body-parser");
 var methodOverride=require("method-override");
 var dbmiddleware=require("./middleware/dbconnectionmiddlware");
 
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({'extended':'true'})); 			
 app.use(bodyParser.json()); 									
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
@@ -12,6 +13,12 @@ app.use(methodOverride());
 
 app.use(dbmiddleware.dbconnection);
 require('./routes/Userroutes')(app);
+
+
+app.get('*', function(req, res) {    
+    res.sendfile('./public/views/index.html');
+ });
+
 
 app.listen(8080);
 console.log("server is running at http://localhost:8080");
